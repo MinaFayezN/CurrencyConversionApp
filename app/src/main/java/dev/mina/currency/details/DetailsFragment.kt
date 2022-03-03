38ac0edaa9a862e26af41b8de9a03ac38fb5a237
Mina Fayez <MinaFayezN@gmail.com>
2022-03-03 13:42:50 +0200

    - Adding Historic chart

--- app/src/main/java/dev/mina/currency/details/DetailsFragment.kt
+++ app/src/main/java/dev/mina/currency/details/DetailsFragment.kt
@@ -1,16 +1,31 @@
 package dev.mina.currency.details
 
+import android.graphics.Color
+import android.graphics.DashPathEffect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.*
+import com.github.mikephil.charting.components.Legend.LegendForm
+import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import dagger.hilt.android.AndroidEntryPoint
+import dev.mina.currency.R
 import dev.mina.currency.databinding.FragmentDetailsBinding
 import dev.mina.currency.utils.observeForSingleEvent
-import java.math.BigDecimal
+
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
@@ -30,6 +45,8 @@ class DetailsFragment : Fragment() {
     private val viewModel: DetailsViewModel by viewModels()
     private val viewState: DetailsViewState by lazy { DetailsViewState() }
 
+    private val chart: LineChart by lazy { binding.chart }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -39,6 +56,7 @@ class DetailsFragment : Fragment() {
             it.viewState = viewState
         }
         uiObservations()
+        createChart()
         return binding.root
     }
 
@@ -52,16 +70,8 @@ class DetailsFragment : Fragment() {
             }
         }
         viewModel.timeSeries.observe(viewLifecycleOwner) { response ->
-            response.rates ?: return@observe
-            response.rates.keys.map {
-                HistoricItem(date = it,
-                    fromText = arguments.base,
-                    fromRate = "1.0",
-                    toText = arguments.to,
-                    toRate = "${response.rates[it]?.get(arguments.to) ?: BigDecimal(0)}"
-                )
-            }.let(viewState.historicAdapter::updateData)
-
+            viewState.historicAdapter.updateData(response)
+            setData(response)
         }
     }
 
@@ -69,4 +79,125 @@ class DetailsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun createChart() {
+        run {
+            // // Chart Style // //
+
+            // background color
+            chart.setBackgroundColor(Color.WHITE)
+
+            // disable description text
+            chart.description.isEnabled = false
+
+            // enable touch gestures
+            chart.setTouchEnabled(true)
+
+            chart.setDrawGridBackground(false)
+
+            // enable scaling and dragging
+            chart.isDragEnabled = true
+            chart.setScaleEnabled(true)
+            // chart.setScaleXEnabled(true);
+            // chart.setScaleYEnabled(true);
+
+            // force pinch zoom along both axis
+            chart.setPinchZoom(true)
+        }
+        var xAxis: XAxis
+        run {
+            // // X-Axis Style // //
+            xAxis = chart.xAxis
+
+            // vertical grid lines
+            xAxis.enableGridDashedLine(10f, 10f, 0f)
+        }
+        var yAxis: YAxis
+        run {
+            // // Y-Axis Style // //
+            yAxis = chart.axisLeft
+
+            // disable dual axis (only use LEFT axis)
+            chart.axisRight.isEnabled = false
+
+            // horizontal grid lines
+            yAxis.enableGridDashedLine(10f, 10f, 0f)
+        }
+        // draw points over time
+        chart.animateX(1500)
+
+        // get the legend (only possible after setting data)
+        val l: Legend = chart.legend
+
+        // draw legend entries as lines
+        l.form = LegendForm.LINE
+    }
+
+    private fun setData(list: List<HistoricItem>) {
+
+        val values = ArrayList<Entry>()
+        for (i in list.indices) {
+            values.add(Entry(i.toFloat(), list[i].toRate?.toFloat() ?: 0f,
+                ResourcesCompat.getDrawable(resources, android.R.drawable.star_on, null)))
+        }
+        val set1: LineDataSet
+        if (chart.data != null &&
+            chart.data.dataSetCount > 0
+        ) {
+            set1 = chart.data.getDataSetByIndex(0) as LineDataSet
+            set1.values = values
+            set1.notifyDataSetChanged()
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            set1 = LineDataSet(values, "${arguments.base} to ${arguments.to}")
+            set1.setDrawIcons(false)
+
+            // draw dashed line
+            set1.enableDashedLine(10f, 5f, 0f)
+
+            // black lines and points
+            set1.color = Color.BLACK
+            set1.setCircleColor(Color.BLACK)
+
+            // line thickness and point size
+            set1.lineWidth = 1f
+            set1.circleRadius = 3f
+
+
+            // draw points as solid circles
+            set1.setDrawCircleHole(false)
+
+            // customize legend entry
+            set1.formLineWidth = 1f
+            set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+            set1.formSize = 15f
+
+            // text size of values
+            set1.valueTextSize = 9f
+
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f)
+
+            // set the filled area
+            set1.setDrawFilled(true)
+            set1.fillFormatter =
+                IFillFormatter { _, _ -> chart.axisLeft.axisMinimum }
+
+            val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.fade_red)
+            set1.fillDrawable = drawable
+
+            val dataSets = ArrayList<ILineDataSet>()
+            dataSets.add(set1) // add the data sets
+
+            // create a data object with the data sets
+            val data = LineData(dataSets)
+
+            // set data
+            chart.data = data
+        }
+    }
+
+
 }
--- app/src/main/java/dev/mina/currency/details/DetailsViewModel.kt
+++ app/src/main/java/dev/mina/currency/details/DetailsViewModel.kt
@@ -3,11 +3,11 @@ package dev.mina.currency.details
 import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.mina.currency.data.HistoricRate
-import dev.mina.currency.data.TimeSeriesRates
 import dev.mina.currency.utils.SingleEvent
 import dev.mina.currency.utils.trigger
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.math.BigDecimal
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,17 +22,27 @@ class DetailsViewModel @Inject constructor(
     private val _historicalRates = MutableLiveData(HistoricRate())
     val historicalRates: LiveData<HistoricRate> = _historicalRates
 
-    private val _timeSeries = MutableLiveData(TimeSeriesRates())
-    val timeSeries: LiveData<TimeSeriesRates> = _timeSeries
+    private val _timeSeries = MutableLiveData<List<HistoricItem>>()
+    val timeSeries: LiveData<List<HistoricItem>> = _timeSeries
 
 
     init {
         _loading.trigger(true)
         viewModelScope.launch(Dispatchers.IO) {
             val base = state.get<String>("base")
+            val to = state.get<String>("to")
             val symbols = state.get<String?>("symbols")
             val resp = detailsRepo.getTimeSeriesRates(base = base, symbols = symbols)
-            _timeSeries.postValue(resp)
+            resp.rates?.keys?.map {
+                HistoricItem(date = it,
+                    fromText = base,
+                    fromRate = "1.0",
+                    toText = to,
+                    toRate = "${resp.rates[it]?.get(to) ?: BigDecimal(0)}"
+                )
+            }?.also {
+                _timeSeries.postValue(it)
+            }
             _loading.trigger(false)
         }
     }
